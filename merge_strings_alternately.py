46dff0dc6af7a4455612a422b2217734e8a30962
omarNasr1 <khlefaooo@gmail.com>
2023-07-25 13:04:06 +0300

    new version of problem1

--- merge_strings_alternately.py
+++ merge_strings_alternately.py
@@ -1,37 +1,43 @@
 word1 = 'omar'
 word2 = 'na'
-res = 'onmaar'
 
 
-res_list = list()
-if len(word1) == len(word2):
-	for i in range(len(word1)):
-		var = word1[i] + word2[i]
-		res_list.append(var)
 
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res_list = list()
+		#case 1 - same lenght 
+        if len(word1) == len(word2):
+            for i in range(len(word1)):
+                var = word1[i] + word2[i]
+                res_list.append(var)
 
-	for letters in res_list:
-		print(str(letters), end='')
+            for letters in res_list:
+                print(str(letters), end='')
+		#case 2 - one word is longer
+        elif len(word2) > len(word1):
+            for i in range(len(word1)):
+                var = word1[i] + word2[i]
+                res_list.append(var)
 
-elif len(word2) > len(word1):
-	for i in range(len(word1)):
-		var = word1[i] + word2[i]
-		res_list.append(var)
+            res_list.append(word2[len(word1)::])
 
-	res_list.append(word2[len(word1)::])
+            for i in res_list:
+                print(str(i), end='')
+                
+                
+		#case 3 - the other one is longer
+        else:
+            for i in range(len(word2)):
+                var = word1[i] + word2[i]
+                res_list.append(var)
 
-	for i in res_list:
-		print(str(i), end='')
-		
-		
+            res_list.append(word1[len(word2)::])
 
-else:
-	for i in range(len(word2)):
-		var = word1[i] + word2[i]
-		res_list.append(var)
-
-	res_list.append(word1[len(word2)::])
-
-	for i in res_list:
-		print(str(i), end='')
+            for i in res_list:
+                print(str(i), end='')
+            
+        
+        result = ''.join(res_list)
+        return result
 	
